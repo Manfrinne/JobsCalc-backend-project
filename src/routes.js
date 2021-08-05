9abe1fd91a0c6d392cc5e4a37292656a5f97336a
Manfrinne <m4nfrinne@gmail.com>
2021-08-05 12:23:03 -0300

    feat: Create server NodeJS and configure presentation routes for EJS files;

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const routes = express.Router();
+
+const views = __dirname + '/views/'
+
+routes.get("/", (req, res) => {
+  return res.render(views + 'index');
+});
+
+routes.get("/job", (req, res) => {
+  return res.render(views + 'job');
+});
+
+routes.get("/jobEdit", (req, res) => {
+  return res.render(views + 'job-edit');
+});
+
+routes.get("/profile", (req, res) => {
+  return res.render(views + 'profile');
+});
+
+module.exports = routes;
